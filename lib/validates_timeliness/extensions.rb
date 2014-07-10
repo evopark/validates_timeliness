db417b10367df543fdc9878dd9c7432c8988319b
Marcus Ilgner <mail@marcusilgner.com>
2014-07-10 17:16:50 +0200

    Make it work without ActiveRecord

--- lib/validates_timeliness/extensions.rb
+++ lib/validates_timeliness/extensions.rb
@@ -1,29 +1,47 @@
 module ValidatesTimeliness
+  module ExtensionLoader
+    # makes sure that extensions which reference unloaded modules still work
+    # that way it's still possible to use the gem w/o ActiveRecord
+    def self.name_error_guard
+      begin
+        yield
+      rescue NameError => e
+        warn("Some extension could not be loaded: #{e}")
+      end
+    end
+  end
+
   module Extensions
     autoload :DateTimeSelect,         'validates_timeliness/extensions/date_time_select'
-    if ActiveRecord::VERSION::MAJOR < 4
-      autoload :MultiparameterHandler, 'validates_timeliness/extensions/multiparameter_handler'
-    else
-      autoload :AttributeAssignment,     'validates_timeliness/extensions/attribute_assignment'
-      autoload :MultiparameterAttribute, 'validates_timeliness/extensions/multiparameter_attribute'
-    end
+    ValidatesTimeliness::ExtensionLoader.name_error_guard {
+      if ActiveRecord::VERSION::MAJOR < 4
+        autoload :MultiparameterHandler, 'validates_timeliness/extensions/multiparameter_handler'
+      else
+        autoload :AttributeAssignment,     'validates_timeliness/extensions/attribute_assignment'
+        autoload :MultiparameterAttribute, 'validates_timeliness/extensions/multiparameter_attribute'
+      end
+    }
   end
 
   def self.enable_date_time_select_extension!
-
-    if ActiveRecord::VERSION::MAJOR < 4
-      ::ActionView::Helpers::InstanceTag.send(:include, ValidatesTimeliness::Extensions::DateTimeSelect)
-    else
-      ::ActionView::Helpers::Tags::DateSelect.send(:include, ValidatesTimeliness::Extensions::DateTimeSelect)
-    end
+    ValidatesTimeliness::ExtensionLoader.name_error_guard {
+      if ActiveRecord::VERSION::MAJOR < 4
+        ::ActionView::Helpers::InstanceTag.send(:include, ValidatesTimeliness::Extensions::DateTimeSelect)
+      else
+        ::ActionView::Helpers::Tags::DateSelect.send(:include, ValidatesTimeliness::Extensions::DateTimeSelect)
+      end
+    }
   end
 
   def self.enable_multiparameter_extension!
-    if ActiveRecord::VERSION::MAJOR < 4
-      ::ActiveRecord::Base.send(:include, ValidatesTimeliness::Extensions::MultiparameterHandler)
-    else
-      ::ActiveRecord::Base.send(:include, ValidatesTimeliness::Extensions::AttributeAssignment)
-      ::ActiveRecord::AttributeAssignment::MultiparameterAttribute.send(:include, ValidatesTimeliness::Extensions::MultiparameterAttribute)
-    end
+    ValidatesTimeliness::ExtensionLoader.name_error_guard {
+      if ActiveRecord::VERSION::MAJOR < 4
+        ::ActiveRecord::Base.send(:include, ValidatesTimeliness::Extensions::MultiparameterHandler)
+      else
+        ::ActiveRecord::Base.send(:include, ValidatesTimeliness::Extensions::AttributeAssignment)
+        ::ActiveRecord::AttributeAssignment::MultiparameterAttribute.send(:include, ValidatesTimeliness::Extensions::MultiparameterAttribute)
+      end
+    }
   end
+
 end
